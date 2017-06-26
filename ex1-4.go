8343fa7d7941ef7bbc8e9665389075f93ca34f71
Joona Kannisto <joonak@dyn-176-246.public.tut.fi>
2017-06-26 15:59:42 +0300

    AAA

--- ex1-4.go
+++ ex1-4.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Printf("%s \n",b)
 		
 	for index := range b {
-		// | binary AND operation 
+		// | binary OR operation 
 		b[index] = b[index]|0x20	
 	}
 	fmt.Printf("%s \n",b)
