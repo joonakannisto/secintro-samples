a4a1f6e7b5a848dcd4ab4209b9223a57facfa204
Markku Vajaranta <markku.vajaranta@tut.fi>
2017-11-06 15:24:52 +0200

    ex7 dh python

---
+++ ex7-dh.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import math
+
+PRIME = 4294967087
+GENERATOR = 2
+
+print PRIME
+print GENERATOR
+
+# Mark = M
+# Jonathan = J
+
+M_Secret = 89
+
+print M_Secret
+
+M_Comp_1 = (math.pow(GENERATOR,M_Secret) % PRIME )
+
+print M_Comp_1
